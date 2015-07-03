88d6ab8c9c93c1784b11f206e830e307bf687b3d
AKIRA-MIAYEK <akira.aktk.miyake@gmail.com>
2015-07-03 23:04:09 +0900

    Initial Commit

---
+++ SFObjectSwift/SFObjectSwift.h
@@ -0,0 +1,19 @@
+//
+//  SFObjectSwift.h
+//  SFObjectSwift
+//
+//  Created by MiyakeAkira on 2015/07/03.
+//  Copyright (c) 2015年 Miyake Akira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFObjectSwift.
+FOUNDATION_EXPORT double SFObjectSwiftVersionNumber;
+
+//! Project version string for SFObjectSwift.
+FOUNDATION_EXPORT const unsigned char SFObjectSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFObjectSwift/PublicHeader.h>
+
+
